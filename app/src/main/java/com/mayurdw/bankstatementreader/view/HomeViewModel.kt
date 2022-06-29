456458ed41860d74ddb7824c05cf2ffec81befea
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2022-06-29 21:24:08 +1200

    Layout of the home screen sorted

--- app/src/main/java/com/mayurdw/bankstatementreader/view/HomeViewModel.kt
+++ app/src/main/java/com/mayurdw/bankstatementreader/view/HomeViewModel.kt
@@ -1,20 +1,24 @@
 package com.mayurdw.bankstatementreader.view
 
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mayurdw.bankstatementreader.Repository
+import java.time.Month
 
 class HomeViewModel : ViewModel() {
     /* MutableLiveData */
     private val _totalExpenses : MutableLiveData<String> = MutableLiveData()
     private val _totalIncome : MutableLiveData<String> = MutableLiveData()
-
+    private val _month : MutableLiveData<Month> = MutableLiveData(Month.APRIL)
     /* Live Data */
     val totalExpenses : LiveData<String>
         get() = this._totalExpenses
     val totalIncome : LiveData<String>
         get() = this._totalIncome
+    val month: LiveData<Month>
+        get() = this._month
 
     init {
         // Load data
