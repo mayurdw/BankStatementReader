0266f2cc6200a980484d380ff729dd33a957882c
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2022-07-11 22:03:29 +1200

    Action bar added & got navigation working

---
+++ app/src/main/java/com/mayurdw/bankstatementreader/view/AddFileViewModel.kt
@@ -0,0 +1,7 @@
+package com.mayurdw.bankstatementreader.view
+
+import androidx.lifecycle.ViewModel
+
+class AddFileViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/mayurdw/bankstatementreader/view/MainActivity.kt
+++ app/src/main/java/com/mayurdw/bankstatementreader/view/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.mayurdw.bankstatementreader.view
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
 import com.mayurdw.bankstatementreader.R
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -12,5 +20,27 @@ class MainActivity : AppCompatActivity() {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        NavigationUI.setupWithNavController(toolbar, navHostFragment.navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return item.onNavDestinationSelected(
+            findNavController(R.id.nav_host_fragment)
+        ) || super.onOptionsItemSelected(item)
     }
 }
